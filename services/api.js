a57888fe7077e3474ab87b827c554c13f389d337
ramalakshmi0304 <mlaksmipradeep@gmail.com>
2026-03-30 17:04:16 +0530

    file updated

--- services/api.js
+++ services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { supabase } from '../src/config/supabaseClient'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
 });
 
 // Automatically add the Supabase token to every request
